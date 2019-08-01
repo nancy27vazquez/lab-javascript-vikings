5e34ce9884f9972fd8b9defa30823ca258cb5fac
Nancy Vázquez Romero <imac_nan@iMac-de-Nancy.local>
2019-07-31 23:47:16 -0500

    80% excercise solved

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,57 @@
+let damage = 50;
+
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }  
+  attack(){
+    return this.strength;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength);
+    this.name = name;
+  }
+  battleCry(){
+    return "Odin Owns You All!";
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+    return "";
+  }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor(health, strength) {
+    super(health, strength);
+  }
+
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(Viking){
+  }
+  addSaxon(Saxon){
+  }
+  vikingAttack(){
+  }
+  saxonAttack(){}
+  showStatus(){}
+}
+
+/*================================== */
